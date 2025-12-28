b9efe81ecc54d60087146bc033fa84e21b8f7ea8
Asmi Panigrahi <52307715+Asmi8@users.noreply.github.com>
2025-12-28 15:30:03 -0500

    Update solution_02.cpp

--- qwen/solution_02.cpp
+++ qwen/solution_02.cpp
@@ -1,1 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+    int n, q;
+    cin >> n >> q;
+    vector<ll> a(n + 1);
+    for (int i = 1; i <= n; i++) cin >> a[i];
+
+    while (q--) {
+        int p;
+        ll x;
+        cin >> p >> x;
+        a[p] = x;
+
+        ll cur = 0;
+        int ans = 0;
+        for (int i = 1; i <= n; i++) {
+            cur += (i & 1) ? a[i] : -a[i];
+            if (cur >= 0) ans++;
+        }
+        cout << ans << "\n";
+    }
+}
 
