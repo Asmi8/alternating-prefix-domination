a15cd881afaa511d95795a942e4ed3811e00830b
Asmi Panigrahi <52307715+Asmi8@users.noreply.github.com>
2025-12-28 15:15:41 -0500

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -1,1 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct SegmentTree {
+    // stores minimum prefix sum
+    // supports range add
+};
+
+int main() {
+    read n, q
+    read array a
+
+    build initial alternating prefix sums
+    build segment tree over them
+
+    for each query:
+        compute delta
+        apply range update to segment tree
+        count good indices
+        print answer
+}
 
